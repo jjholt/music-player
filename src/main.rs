d0a909a3e708eb474576be6e6d9a5a06af9f2b79
JJ <jonathanjamesholt@gmail.com>
2026-08-09 20:41:46 +0100

    fix: use modes in the global key handler

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use std::time::Duration;
 use crate::config::Config;
 use crate::mpd::MpdClient;
 use crate::tui::TuiGuard;
-use crate::ui::{ActiveView, App};
+use crate::ui::{ActiveView, App, Mode};
 
 enum AppEvent {
     Key(crossterm::event::KeyEvent),
@@ -69,10 +69,11 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn handle_global_key(app: &mut App<MpdClient>, key: crossterm::event::KeyEvent) -> bool {
-    if app.command_bar.active {
-        app.handle_key(key);
-    } else {
-        match (key.modifiers, key.code) {
+    match app.mode {
+        Mode::Input => {
+            app.handle_key(key);
+        }
+        Mode::Normal => match (key.modifiers, key.code) {
             (KeyModifiers::NONE, KeyCode::Char('q')) => return true,
             (KeyModifiers::CONTROL, KeyCode::Enter) => {
                 app.trigger_database_update();
@@ -101,7 +102,7 @@ fn handle_global_key(app: &mut App<MpdClient>, key: crossterm::event::KeyEvent) 
             _ => {
                 app.handle_key(key);
             }
-        }
-    };
+        },
+    }
     false
 }
